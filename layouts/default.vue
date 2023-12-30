e3c979f3e2dc93912c564d297225f9bbf48e192e
kaskazini <kaskazinisolutions>
2023-12-30 13:21:11 +0300

    udpated default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,9 @@
 <template>
   <html lang="en" dir="ltr" data-mode="light" class="scroll-smooths group" data-theme-color="blue">
 
-  <body class="bg-white dark:bg-neutral-800 min-h-screen">
+  <div class="bg-white dark:bg-neutral-800 min-h-screen">
     <slot />
-  </body>
+  </div>
 
 </html>
 </template>
